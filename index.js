dbd7d6e19ed67213d77d3b39a2e3378c3a0dae12
Shoyeb Mohammed Suny <69183694+sunywebdev@users.noreply.github.com>
2023-01-02 00:40:41 +0600

    email added

--- index.js
+++ index.js
@@ -17,6 +17,13 @@ app.use(express.json());
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@sunywebdev.7tytk.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri);
 
+///email settings
+const Sib = require("sib-api-v3-sdk");
+const emailclient = Sib.ApiClient.instance;
+const apiKey = emailclient.authentications["api-key"];
+apiKey.apiKey = process.env.API_KEY_SENDINBLUE;
+const tranEmailApi = new Sib.TransactionalEmailsApi();
+
 async function run() {
 	try {
 		await client.connect();
@@ -36,17 +43,13 @@ async function run() {
 		//To add new user when login or signup
 		app.post("/users", async (req, res) => {
 			const newuser = req.body;
-			console.log("Request from UI ", newuser);
 			const result = await usersCollection.insertOne(newuser);
-			console.log("Successfully Added New User ", result);
 			res.json(result);
 		});
 		//To update or replace users data when login or signup
 		app.put("/users", async (req, res) => {
-			console.log(req.body);
 			const user = req.body;
 			const filter = { email: user?.email };
-			console.log("Request to replace or add user", user);
 			const options = { upsert: true };
 			const updateuser = {
 				$set: {
@@ -61,22 +64,17 @@ async function run() {
 				options,
 			);
 			res.json(result);
-			console.log("Successfully replaced or added user", result);
 		});
 		//Check Admin or Not
 		app.get("/users/:email", async (req, res) => {
 			const email = req.params.email;
-			console.log("from UI", email);
 			const filter = { email: email };
-			console.log("Request to find ", filter);
 			const user = await usersCollection.findOne(filter);
-			console.log(user);
 			let isAdmin = false;
 			if (user?.userRole === "Admin") {
 				isAdmin = true;
 			}
 			res.json({ admin: isAdmin });
-			console.log("Found one", user);
 		});
 
 		/* ------
@@ -86,42 +84,60 @@ async function run() {
 		//To post new mail
 		app.post("/mails", async (req, res) => {
 			const newMail = req.body;
-			console.log("Request from UI ", newMail);
+			tranEmailApi.sendTransacEmail({
+				sender: {
+					email: newMail?.userEmail,
+					name: newMail?.userName,
+				},
+				to: [{ email: "suny.webdev@gmail.com" }],
+				subject: `New Mail From ${newMail?.userName} in Portfolio.`,
+				htmlContent: `
+				<h4>Name :</h4> <span>${newMail?.userName}</span><br/>
+				<h4>Email :</h4> <span>${newMail?.userEmail}</span><br/>
+				<h4>Subject :</h4> <span>${newMail?.subject}</span><br/>
+				<h4>Message :</h4> <span>${newMail?.message}</span><br/>
+		`,
+			});
 			const result = await mailsCollection.insertOne(newMail);
-			console.log("Successfully Added new Mail ", result);
 			res.json(result);
 		});
 		//To post new reviews
 		app.post("/reviews", async (req, res) => {
 			const newReviews = req.body;
-			console.log("Request from UI ", newReviews);
+			tranEmailApi.sendTransacEmail({
+				sender: {
+					email: newReviews?.userEmail,
+					name: newReviews?.userName,
+				},
+				to: [{ email: "suny.webdev@gmail.com" }],
+				subject: `New ${newReviews?.star} Star Reviews From ${newReviews?.userName} in Portfolio.`,
+				htmlContent: `
+				<h4>Name :</h4> <span>${newReviews?.userName}</span><br/>
+				<h4>Email :</h4> <span>${newReviews?.userEmail}</span><br/>
+				<h4>Rating Star :</h4> <span>${newReviews?.star}</span><br/>
+				<h4>Review :</h4> <span>${newReviews?.userReview}</span><br/>
+		`,
+			});
 			const result = await reviewsCollection.insertOne(newReviews);
-			console.log("Successfully Added New reviews ", result);
 			res.json(result);
 		});
 		//To post new blog
 		app.post("/blogs", async (req, res) => {
 			const newBlog = req.body;
-			console.log("Request from UI ", newBlog);
 			const result = await blogsCollection.insertOne(newBlog);
-			console.log("Successfully Added New Blog ", result);
 			res.json(result);
 		});
 		//To post new projects
 		app.post("/projects", async (req, res) => {
 			const newProject = req.body;
-			console.log("Request from UI ", newProject);
 			const result = await projectsCollection.insertOne(newProject);
-			console.log("Successfully Added New Project ", result);
 			res.json(result);
 		});
 		// To store/update single project data
 		app.put("/projects/:id", async (req, res) => {
 			const id = req.params.id;
-			console.log("Request to update ", id);
 			const projectId = { _id: ObjectId(id) };
 			const updatedReq = req.body;
-			console.log("Comming form UI", updatedReq);
 			const options = { upsert: true };
 			const updateProject = {
 				$set: {
@@ -153,7 +169,6 @@ async function run() {
 				options,
 			);
 			res.json(result);
-			console.log("Updated Successfully", result);
 		});
 
 		/* ------
@@ -162,57 +177,40 @@ async function run() {
 
 		//To Show all users
 		app.get("/users", async (req, res) => {
-			console.log(req.query);
 			const get = usersCollection.find({});
-			console.log("Request to find users");
 			users = await get.toArray();
 			res.send(users);
-			console.log("Found all users", users);
 		});
 		//To Show all projects
 		app.get("/projects", async (req, res) => {
-			console.log(req.query);
 			const get = projectsCollection.find({});
-			console.log("Request to find projects");
 			projects = await get.toArray();
 			res.send(projects);
-			console.log("Found all projects", projects);
 		});
 		//To Show all reviews
 		app.get("/reviews", async (req, res) => {
-			console.log(req.query);
 			const get = reviewsCollection.find({});
-			console.log("Request to find reviews");
 			reviews = await get.toArray();
 			res.send(reviews);
-			console.log("Found all reviews", reviews);
 		});
 		//To Show all Blogs
 		app.get("/blogs", async (req, res) => {
-			console.log(req.query);
 			const get = blogsCollection.find({});
-			console.log("Request to find blogs");
 			blogs = await get.toArray();
 			res.send(blogs);
-			console.log("Found all blogs", blogs);
 		});
 		//To Show all mails
 		app.get("/mails", async (req, res) => {
-			console.log(req.query);
 			const get = mailsCollection.find({});
-			console.log("Request to find mails");
 			mails = await get.toArray();
 			res.send(mails);
-			console.log("Found all mails", mails);
 		});
 		//To load single project by id
 		app.get("/projects/:id", async (req, res) => {
 			const id = req.params.id;
-			console.log("Request to find ", id);
 			const findId = { _id: ObjectId(id) };
 			const result = await projectsCollection.findOne(findId);
 			res.send(result);
-			console.log("Found one", result);
 		});
 
 		/* ------
@@ -222,39 +220,31 @@ async function run() {
 		//To Delete user one by one
 		app.delete("/users/:id", async (req, res) => {
 			const id = req.params.id;
-			console.log("Request to delete ", id);
 			const deleteId = { _id: ObjectId(id) };
 			const result = await usersCollection.deleteOne(deleteId);
 			res.send(result);
-			console.log("user Successfully Deleted", result);
 		});
-		
+
 		//To Delete projects one by one
 		app.delete("/projects/:id", async (req, res) => {
 			const id = req.params.id;
-			console.log("Request to delete ", id);
 			const deleteId = { _id: ObjectId(id) };
 			const result = await projectsCollection.deleteOne(deleteId);
 			res.send(result);
-			console.log("projects Successfully Deleted", result);
 		});
 		//To Delete reviews one by one
 		app.delete("/reviews/:id", async (req, res) => {
 			const id = req.params.id;
-			console.log("Request to delete ", id);
 			const deleteId = { _id: ObjectId(id) };
 			const result = await reviewsCollection.deleteOne(deleteId);
 			res.send(result);
-			console.log("reviews Successfully Deleted", result);
 		});
 		//To Delete mails one by one
 		app.delete("/mails/:id", async (req, res) => {
 			const id = req.params.id;
-			console.log("Request to delete ", id);
 			const deleteId = { _id: ObjectId(id) };
 			const result = await mailsCollection.deleteOne(deleteId);
 			res.send(result);
-			console.log("mails Successfully Deleted", result);
 		});
 	} finally {
 		//await client.close();
